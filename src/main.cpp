049264a9d3576c02be466cb5f9aeac2cc509aff5
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-04-26 22:27:33 +0100

    refactor(main-cmake): clean `main.cpp` to use Game object and update `CMake` source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Vyboros Snake - Main Window");
-
-    while (window.isOpen()) {
-        // SFML 3 -> events
-        while (const auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
-
-        window.clear(sf::Color::Black);
-        window.display();
-    }
-
+    Game game;
+    game.run();
     return 0;
 }
